d18b140a6178a4c7396b27a2a04304ecfaf74c90
Cedrus81 <erez.eitan.97@gmail.com>
2024-09-30 11:09:25 +0300

    started work on the linear humidity bar

---
+++ src/app/_components/highlights/HumidityBar.tsx
@@ -0,0 +1,13 @@
+function HumidityBar() {
+  return (
+    <div className="bar-container">
+      <div className="bar-scales"></div>
+      <div className="whitebar">
+        <div className="fill-bar"></div>
+      </div>
+      <span></span>
+    </div>
+  );
+}
+
+export default HumidityBar;
